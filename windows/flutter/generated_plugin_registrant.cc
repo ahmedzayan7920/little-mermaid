4888fe94269b4472e10e24c435bc141552eea321
Ahmed Zayan <ahmedabdallazayan@gmail.com>
2024-12-24 21:51:56 +0200

    Add multiple dependencies and update plugin registrants
    Added several new dependencies to `pubspec.yaml` and `pubspec.lock`, including `flutter_bloc`, `get_it`, `zego_uikit_prebuilt_call`, `zego_uikit_signaling_plugin`, and others. Updated the `generated_plugin_registrant.cc` and `generated_plugins.cmake` files to include the necessary plugins for the new dependencies.

--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -14,6 +14,8 @@
 #include <firebase_storage/firebase_storage_plugin_c_api.h>
 #include <permission_handler_windows/permission_handler_windows_plugin.h>
 #include <rive_common/rive_plugin.h>
+#include <zego_express_engine/zego_express_engine_plugin.h>
+#include <zego_zim/zego_zim_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
   AudioplayersWindowsPluginRegisterWithRegistrar(
@@ -32,4 +34,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("PermissionHandlerWindowsPlugin"));
   RivePluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("RivePlugin"));
+  ZegoExpressEnginePluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ZegoExpressEnginePlugin"));
+  ZegoZimPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("ZegoZimPlugin"));
 }
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -11,6 +11,8 @@ list(APPEND FLUTTER_PLUGIN_LIST
   firebase_storage
   permission_handler_windows
   rive_common
+  zego_express_engine
+  zego_zim
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
